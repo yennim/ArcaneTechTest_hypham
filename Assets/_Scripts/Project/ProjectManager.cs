85f344e1fb44843cd5c9de85b0816f8997d8ad70
yennim <hng.yen.pham@gmail.com>
2025-11-20 11:28:54 -0500

    Add editing project name
    - Edit on click, used ugly placeholder

--- Assets/_Scripts/Project/ProjectManager.cs
+++ Assets/_Scripts/Project/ProjectManager.cs
@@ -8,8 +8,7 @@ public class ProjectManager : MonoBehaviour
     private Project currentProject;
     private List<Project> projects;
 
-    [SerializeField] private Transform room;
-    [SerializeField] private List<string> projectNames;
+    [SerializeField] private Transform modelsContainer;
 
     // I decided to only consider one project at a time, no multiple tabs to edit projects. Makes it easier to find the instance.
     public static ProjectManager Instance;
@@ -30,22 +29,29 @@ public class ProjectManager : MonoBehaviour
 
     void Start()
     {
+        // load saved projects
+        projects = new List<Project>();
+
         currentProject = new Project(name);
+    }
+    public void EditProjectName(string name)
+    {
+        currentProject.Name = name;
     }
 
     public void SaveProject()
     {
-
+        projects.Add(currentProject);
     }
     
     public void LoadProject(string id)
     {
-
+        currentProject = projects.Find(p => p.Id == id);
     }
 
     public void AddModel(ModelTypeStruct modelType)
     {
-        Instantiate(modelType.Prefab, room);
+        Instantiate(modelType.Prefab, modelsContainer);
     }
 
     public void SetSelectedModel(ModelController modelController)
---
+++ Assets/_Scripts/UI/EditableText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditableText : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] TMP_InputField inputField;
+
+    public void EditText()
+    {
+        button.gameObject.SetActive(false);
+        inputField.text = text.text;
+        inputField.gameObject.SetActive(true);
+    }
+
+    public void SaveEdit()
+    {
+        text.text = inputField.text;
+        inputField.gameObject.SetActive(false);
+        button.gameObject.SetActive(true);
+    }
+}
