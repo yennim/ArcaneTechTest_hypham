b583d0cd5ad6a8bedcf6b123409f44675644ec98
yennim <hng.yen.pham@gmail.com>
2025-11-21 09:37:12 -0500

    Add preview on begin and on drag
    - Reuse same preview prefab if created once instead of destroy and instantiate

--- Assets/_Scripts/UI/UIModel/ButtonDragAndDropModel.cs
+++ Assets/_Scripts/UI/UIModel/ButtonDragAndDropModel.cs
@@ -1,13 +1,12 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static TreeEditor.TreeEditorHelper;
-using UnityEngine.UIElements;
 
 public class ButtonDragAndDropModel : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private LayerMask droppableLayerMasks;
+    [SerializeField] private float previewFloatingDistance = 11f;
 
     private Camera mainCamera;
     private GameObject modelPreview;
@@ -27,21 +26,57 @@ public class ButtonDragAndDropModel : MonoBehaviour, IBeginDragHandler, IDragHan
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
+        Ray ray = mainCamera.ScreenPointToRay(eventData.position);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, droppableLayerMasks))
+        {
+            InstantiateOrReactivate(hit.point);
+        }
+        else
+        {
+            InstantiateOrReactivate(ray.GetPoint(previewFloatingDistance));
+        }
+    }
+
+    private void InstantiateOrReactivate(Vector3 position)
+    {
+        if (modelPreview)
+        {
+            modelPreview.SetActive(true);
+            modelPreview.transform.position = position;
+        }
+        else
+        {
+            modelPreview = Instantiate(Model.Prefab.gameObject, position, new Quaternion(0, 0.382683426f, 0, 0.923879564f)); // ugly but copied from inspector        
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (modelPreview)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(eventData.position);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, droppableLayerMasks))
+            {
+                modelPreview.transform.position = hit.point;
+            }
+            else
+            {
+                modelPreview.transform.position = ray.GetPoint(previewFloatingDistance);
+            }
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Ray ray = mainCamera.ScreenPointToRay(eventData.position);
-        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 100f, droppableLayerMasks))
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, droppableLayerMasks))
         {
             ProjectManager.Instance.AddModel(Model, hit.point);
         }
+
+        modelPreview.SetActive(false);
     }
 }
